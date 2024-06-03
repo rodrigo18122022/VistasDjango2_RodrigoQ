b3b55c561947b2739d83bdefead57795e79fc769
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-03 10:30:12 -0500

    Cambiando el modelo de personas y migrando

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombres = models.TextField()
+    apellidos = models.TextField()
+    edad = models.TextField()
