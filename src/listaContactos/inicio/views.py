88d4c3e4ed05094341ed4d35d0054af4df40edb9
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-04 13:25:11 -0500

    Creacion de templates 1 para home.html

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(*args, **kwargs):
-    return HttpResponse('<h1>Hola Mundo desde DJango</h1>')
+def myHomeView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request,"home.html",{})
     
 def anotherView(request):
     return HttpResponse('<h1>Solo otra pagina</h1>')
