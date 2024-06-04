4eb7e628baa9ed5ec98927af02c79f04103645c1
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-04 10:15:32 -0500

    Cambiando los tipos de campos de nombre, apellidos, edad de personas y migrando

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
+    nombre = models.CharField(max_length = 100)
+    apellidos = models.CharField(max_length = 100)
+    edad = models.IntegerField()#(max_digits=3)
