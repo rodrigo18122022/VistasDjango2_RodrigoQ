981acc640aec9993d7ed296212525816edc24c2f
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-04 13:11:35 -0500

    Creacion de mas vistas, en este caso another con su URL

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
     return HttpResponse('<h1>Hola Mundo desde DJango</h1>')
+    
+def anotherView(request):
+    return HttpResponse('<h1>Solo otra pagina</h1>')
