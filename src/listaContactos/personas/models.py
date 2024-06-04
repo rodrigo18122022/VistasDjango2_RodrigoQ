873289fae2c0aafdc0b17097d1f6a16c438cd746
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-04 11:02:34 -0500

    Cambiando el models, ahora hay un BoolanField para donador

--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -5,3 +5,4 @@ class Persona(models.Model):
     nombre = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad = models.IntegerField()#(max_digits=3)
+    donador = models.BooleanField()
